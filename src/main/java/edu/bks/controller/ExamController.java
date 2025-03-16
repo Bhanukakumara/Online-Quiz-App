f68b3f51b760d47778fd15ee8b97f2bc43a306e1
Bhanukakumara <bhanukarasika@gmail.com>
2025-03-16 15:56:55 +0530

    exam create and get exam by course id are completed

--- src/main/java/edu/bks/controller/ExamController.java
+++ src/main/java/edu/bks/controller/ExamController.java
@@ -2,13 +2,8 @@ package edu.bks.controller;
 
 import edu.bks.dto.Exam;
 import edu.bks.service.ExamService;
-import edu.bks.service.impl.ExamServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exam")
@@ -20,5 +15,10 @@ public class ExamController {
     @PostMapping("/create-exam")
     public void createExam(@RequestBody Exam exam){
         service.createExam(exam);
+    }
+
+    @GetMapping("/get-exam-by-course-id/{id}")
+    public void getExamByCourseId(@PathVariable Integer id){
+        service.getExamByCourseId(id);
     }
 }
---
+++ src/main/java/edu/bks/entity/ExamEntity.java
@@ -0,0 +1,23 @@
+package edu.bks.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "exam")
+public class ExamEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer courseId;
+    private String timeLimit;
+    private Integer passingScope;
+    private Integer questionCount;
+}

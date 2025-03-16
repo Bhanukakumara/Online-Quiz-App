dbba991578a0795daee4ec62115c3fba2d7c4538
Bhanukakumara <bhanukarasika@gmail.com>
2025-03-16 13:30:29 +0530

    exam controller update

--- src/main/java/edu/bks/controller/ExamController.java
+++ src/main/java/edu/bks/controller/ExamController.java
@@ -1,6 +1,10 @@
 package edu.bks.controller;
 
 import edu.bks.dto.Exam;
+import edu.bks.service.ExamService;
+import edu.bks.service.impl.ExamServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/exam")
+@RequiredArgsConstructor
 public class ExamController {
+
+    final ExamService service;
+
     @PostMapping("/create-exam")
     public void createExam(@RequestBody Exam exam){
-        
+        service.createExam(exam);
     }
 }
